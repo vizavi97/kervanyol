b1e6947edd36b299efa75249eea38f5d4664e792
vizavi97 <peajiucm@yahoo.com>
2020-05-28 12:48:16 +0500

    28.05

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 import $ from "jquery";
-import bootstrap from "bootstrap";
-import 'owl.carousel2/dist/owl.carousel';
-
+import 'bootstrap'
+import 'owl.carousel2/dist/owl.carousel'
+import Drift from "drift-zoom/src/js/Drift";
+import Swiper from "swiper";
 import categories from '../json/categories'
 
 const bgSwitcher = document.getElementById('bg-switcher');
@@ -13,31 +14,31 @@ if (bgSwitcher) {
 }
 
 $('.top-slider').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots: true,
-    dotsEach: 4,
-    center: true,
-    responsiveClass: true,
-    autoplay: true,
-    autoplayTimeout: 10000,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 1,
-      },
-      1000: {
-        items: 1,
-      }
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: true,
+  dotsEach: 4,
+  center: true,
+  responsiveClass: true,
+  autoplay: true,
+  autoplayTimeout: 10000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
     }
+  }
 });
 $('.recent-products-items').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
+  loop: true,
+  margin: 10,
+  nav: true,
   autoplay: true,
   navText: [" ", " "],
   autoplayTimeout: 10000,
@@ -54,9 +55,9 @@ $('.recent-products-items').owlCarousel({
   }
 });
 $('.popular-products-categories').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
+  loop: true,
+  margin: 10,
+  nav: true,
   dots: true,
   autoplay: false,
   navText: [" ", " "],
@@ -77,9 +78,60 @@ const popularProductsWrapper = document.getElementById('popularProductsWrapper')
 if (popularProductsWrapper) {
   const popularProductsDots = popularProductsWrapper.querySelector('.owl-dots');
   document.addEventListener('DOMContentLoaded', () => {
-    popularProductsDots.innerHTML = categories.map((elem,index) =>
+    popularProductsDots.innerHTML = categories.map((elem, index) =>
       `<div class="owl-dot${index == 0 ? ' active' : ''}">${elem.name}</div>`
     ).join('')
   })
 }
 
+
+// CATALOG-TOGGLER
+document.addEventListener('DOMContentLoaded', () => {
+  const cubeToggler = document.querySelector('.catalog-container-toggler-cube');
+  const lineToggler = document.querySelector('.catalog-container-toggler-line');
+  const toogleWrapper = document.querySelector('.catalog-container-items');
+  if (cubeToggler) {
+    cubeToggler.addEventListener('click', () => {
+      cubeToggler.classList.add('active');
+      lineToggler.classList.remove('active');
+      toogleWrapper.classList.add('cube');
+      toogleWrapper.classList.remove('line');
+    });
+  }
+  if (lineToggler) {
+    lineToggler.addEventListener('click', () => {
+      lineToggler.classList.add('active');
+      cubeToggler.classList.remove('active');
+      toogleWrapper.classList.add('line');
+      toogleWrapper.classList.remove('cube');
+    })
+  }
+});
+
+let demoTrigger = document.querySelector('.swiper-slide-active').querySelector('img');
+let demoCollection = document.querySelector('.product-slider-thumbs-slide-img');
+let paneContainer = document.querySelector('.product-slider-container');
+
+new Drift(demoTrigger, {
+  paneContainer: paneContainer,
+  inlinePane: true,
+  zoomFactor: 1.8,
+});
+new Drift(demoCollection, {
+  paneContainer: paneContainer,
+  inlinePane: false,
+});
+
+var productSliderThumbs = new Swiper('.product-slider-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var productSliderTop = new Swiper('.product-slider-top', {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: productSliderThumbs
+  }
+});
